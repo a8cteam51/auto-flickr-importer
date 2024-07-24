1ad480b49930e3d88b7af742849c3b3711603a6e
ninomihovilic <nino.mihovilic@gmail.com>
2024-07-24 19:04:16 +0800

    Add plugin hooks

---
+++ includes/hooks.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Filters the comment author URL to link to the Flickr user profile.
+ *
+ * @param string             $comment_author_url The comment author URL.
+ * @param integer|WP_Comment $comment_id         The comment ID.
+ *
+ * @return string
+ */
+function wpcomsp_auto_flickr_importer_flickr_comment_author_url( string $comment_author_url, int|WP_Comment $comment_id ): string {
+	$flickr_user_id = get_comment_meta( $comment_id, '_flickr_author_nsid', true );
+
+	if ( ! empty( $flickr_user_id ) ) {
+		return 'https://flickr.com/people/' . $flickr_user_id;
+	} else {
+		return $comment_author_url;
+	}
+}
+add_filter( 'get_comment_author_url', 'wpcomsp_auto_flickr_importer_flickr_comment_author_url', 10, 2 );
