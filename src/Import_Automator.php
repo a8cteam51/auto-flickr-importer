fa39bfd5f7eb50f883f6cb1e254646bfb1028db1
ninomihovilic <nino.mihovilic@gmail.com>
2024-08-09 17:12:44 +0800

    Set automator

--- src/Import_Automator.php
+++ src/Import_Automator.php
@@ -2,8 +2,9 @@
 
 namespace WPCOMSpecialProjects\AutoFlickrImporter;
 
-use WPCOMSpecialProjects\AutoFlickrImporter\Importers\Comment_Importer;
-use WPCOMSpecialProjects\AutoFlickrImporter\Importers\Photo_Stream_Importer;
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Fetch_Comment_Delta_Task;
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Fetch_Latest_Task;
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Initial_Import_Task;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -28,7 +29,7 @@ final class Import_Automator {
 	 *
 	 * @return  void
 	 */
-	public function initialize() {
+	public function initialize(): void {
 		add_action( 'admin_init', array( $this, 'set_flickr_import_cron' ) );
 		add_action( 'run_automate_flickr_import_hook', array( $this, 'run_automate_flickr_import' ) );
 	}
@@ -36,30 +37,76 @@ final class Import_Automator {
 	/**
 	 * Set the Flickr import cron.
 	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 *
 	 * @return void
 	 */
-	public function set_flickr_import_cron() {
-		if ( function_exists( 'as_next_scheduled_action' ) && false === as_next_scheduled_action( 'run_automate_flickr_import_hook' ) ) {
-			as_schedule_recurring_action( time(), 3600, 'run_automate_flickr_import_hook' );
+	public function set_flickr_import_cron(): void {
+
+		$this->start_automated_flickr_import_tasks();
+
+		if ( isset( $_POST['_wpnonce'], $_POST['action'] ) && wp_verify_nonce( wp_unslash( $_POST['_wpnonce'] ), 'initial_import_action' ) && 'run_initial_import' === $_POST['action'] ) {
+			$this->run_initial_import();
+		}
+
+		if ( isset( $_POST['_wpnonce'], $_POST['action'] ) && wp_verify_nonce( wp_unslash( $_POST['_wpnonce'] ), 'initial_import_action' ) && 're-run_initial_import' === $_POST['action'] ) {
+			$this->run_initial_import( true );
 		}
 	}
 
 	/**
 	 * Run the automate Flickr import.
 	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 *
 	 * @return void
 	 */
-	public function run_automate_flickr_import() {
+	private function start_automated_flickr_import_tasks(): void {
 
 		if ( ! wpcomsp_auto_flickr_importer_credentials_exist() ) {
 			return;
 		}
 
-		$photo_stream_importer = new Photo_Stream_Importer();
-		$photo_stream_importer->start_import( 50 );
+		$initial_import_finished = wpcomsp_auto_flickr_importer_get_raw_setting( 'initial_import_finished' );
+		$latest_timestamp        = wpcomsp_auto_flickr_importer_get_raw_setting( 'latest_import_time' );
 
-		$comment_importer = new Comment_Importer();
-		$comment_importer->start_import();
+		if ( false === boolval( $initial_import_finished ) || null === $latest_timestamp ) {
+			return;
+		}
+
+		Fetch_Latest_Task::register_task();
+		Fetch_Comment_Delta_Task::register_task();
+	}
+
+	/**
+	 * Runs the initial import.
+	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param boolean $re_run Whether to re-run the initial import.
+	 *
+	 * @return void
+	 */
+	private function run_initial_import( bool $re_run = false ): void {
+
+		if ( ! wpcomsp_auto_flickr_importer_credentials_exist() ) {
+			return;
+		}
+
+		$initial_import_finished = wpcomsp_auto_flickr_importer_get_raw_setting( 'initial_import_finished' );
+
+		if ( true === boolval( $initial_import_finished ) && false === $re_run ) {
+			return;
+		}
+
+		if ( $re_run ) {
+			wpcomsp_auto_flickr_importer_update_raw_setting( 'initial_import_finished', false );
+		}
+
+		Initial_Import_Task::register_task();
 	}
 
 	// endregion
--- src/Plugin.php
+++ src/Plugin.php
@@ -2,6 +2,10 @@
 
 namespace WPCOMSpecialProjects\AutoFlickrImporter;
 
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Fetch_Comment_Delta_Task;
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Fetch_Latest_Task;
+use WPCOMSpecialProjects\AutoFlickrImporter\Tasks\Initial_Import_Task;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -12,6 +16,16 @@ defined( 'ABSPATH' ) || exit;
  */
 class Plugin {
 	// region FIELDS AND CONSTANTS
+
+	/**
+	 * The background tasks component.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @var   BackgroundTasks|null
+	 */
+	protected ?BackgroundTasks $background_tasks = null;
 
 	// endregion
 
@@ -74,6 +88,18 @@ class Plugin {
 	}
 
 	/**
+	 * Returns the background tasks component.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  BackgroundTasks
+	 */
+	public function get_background_tasks(): BackgroundTasks {
+		return $this->background_tasks;
+	}
+
+	/**
 	 * Initializes the plugin components.
 	 *
 	 * @since   1.0.0
@@ -84,6 +110,18 @@ class Plugin {
 	public function initialize(): void {
 		$settings = new Settings();
 		$settings->initialize();
+
+		$initial_import_task = new Initial_Import_Task();
+		$initial_import_task->initialize();
+
+		$fetch_latest_import_task = new Fetch_Latest_Task();
+		$fetch_latest_import_task->initialize();
+
+		$fetch_comment_delta_import_task = new Fetch_Comment_Delta_Task();
+		$fetch_comment_delta_import_task->initialize();
+
+		$this->background_tasks = new BackgroundTasks();
+		$this->background_tasks->initialize();
 
 		$import_automator = new Import_Automator();
 		$import_automator->initialize();
